119b71096f02c18f7ef479e200afb950961437d6
RynEEvans <ryanee@email.sc.edu>
2025-02-20 01:04:14 -0500

    uploading completed iterator design pattern

--- iterator/Airline.java
+++ iterator/Airline.java
@@ -22,16 +22,19 @@ public class Airline {
     }
 
     public FlightIterator createIterator(String fromCode, String toCode) {
-        Airport from = Airport.valueOf(fromCode.toUpperCase());
-        Airport to = Airport.valueOf(toCode.toUpperCase());
-        ArrayList<Flight> filteredFlights = new ArrayList<>();
-
-        for (Flight flight : flights) {
-            if (flight.getFrom() == from && flight.getTo() == to) {
-                filteredFlights.add(flight);
+        try {
+            Airport from = Airport.valueOf(fromCode.toUpperCase());
+            Airport to = Airport.valueOf(toCode.toUpperCase());
+            ArrayList<Flight> filteredFlights = new ArrayList<>();
+            for (Flight flight : flights) {
+                if (flight.getFrom() == from && flight.getTo() == to) {
+                    filteredFlights.add(flight);
+                }
             }
+            return new FlightIterator(filteredFlights, from, to);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Must enter valid airport codes!");
+            return null;
         }
-
-        return new FlightIterator(filteredFlights, from, to);
     }
 }
